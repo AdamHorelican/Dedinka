0bf42708543d4f7c8c4a748cc0715a81fcb61252
AdamLadnak <a.ladnak25@gmail.com>
2023-01-02 22:14:13 +0100

    .

--- Dedinka/Dedinka.cpp
+++ Dedinka/Dedinka.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include <iomanip> //setw
 #include <Windows.h>
-#include <stdlib.h> //system ("CLS")
+
 
 using namespace std;
 
+bool game_running = true;
+
+int x = 1;
+int y = 1;
+
 char map[19][80] = {
 	"###############################################################################",
-	"#O                                                                            #",
+	"#&                                                                            #",
 	"#                                                                             #",
 	"#                                                                             #",
 	"#                                                                             #",
@@ -49,6 +54,7 @@ int main()
 	"                         << Press ENTER to continue >>                         " };
 
 
+
 	for (int i = 0; i < 19; i++)
 	{
 		cout << setw(90) << right << title[i] << endl;
@@ -57,12 +63,40 @@ int main()
 	if (cin.get() == '\n')
 	{
 		system("CLS");
+	}
+
+	while (game_running = true)
+	{
+		system("CLS");
 		for (int i = 0; i < 19; i++)
 		{
 			cout << setw(90) << right << map[i] << endl;
 		}
 		system("pause>nul");
+		
+		if (GetAsyncKeyState(VK_DOWN)) 
+		{
+			int y2 = y + 1;
+			if (map[y2][x] == ' ')
+			{
+				map[y][x] = ' ';
+				y++;
+				map[y][x] = '&';
+			}
+		}
+		if (GetAsyncKeyState(VK_UP))
+		{
+			int y2 = y - 1;
+			if (map[y2][x] == ' ')
+			{
+				map[y][x] = ' ';
+				y--;
+				map[y][x] = '&';
+			}
+		}
+		
 	}
-	
+
+
 	return 0;
 }
